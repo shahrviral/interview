ba11c550b1101fc555df8f24d736b8adaa6fd823
Viral Shah <viralshah@virals-mbp.lan>
2019-01-25 20:10:14 -0800

    sort

--- strstr.py
+++
@@ -1,9 +0,0 @@
-def strstr(haystack, needle):
-    for i in range(0, len(haystack) - len(needle) + 1):
-        temp = haystack[i: i + len(needle)]
-        if temp == needle:
-            return i
-    return -1
-
-if __name__ == '__main__':
-    print(strstr('lll0', 'lll'))
